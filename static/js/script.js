f8972a96a961c017539841a661dca8709979e282
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-03-23 13:46:38 +0000

    input validation for textarea

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,33 @@ for (let i = 0; i < nav_links.length; i++) {
 function activate() {
     this.classList.add("active")
 }
+
+// https://stackoverflow.com/questions/13643417/how-to-validate-pattern-matching-in-textarea
+// Yann Brelière
+
+$('#add_review').keyup(validateTextarea);
+$('#edit_review').keyup(validateTextarea);
+
+function validateTextarea() {
+        var errorMsg = "No special characters, no multiple spaces, 25-500 characters";
+        var textarea = this;
+        var pattern = new RegExp('^' + $(textarea).attr('pattern') + '$');
+        // check each line of text
+        $.each($(this).val().split("\n"), function () {
+            // check if the line matches the pattern
+            var hasError = !this.match(pattern);
+            if (typeof textarea.setCustomValidity === 'function') {
+                textarea.setCustomValidity(hasError ? errorMsg : '');
+            } else {
+                // Not supported by the browser, fallback to manual error display...
+                $(textarea).toggleClass('error', !!hasError);
+                $(textarea).toggleClass('ok', !hasError);
+                if (hasError) {
+                    $(textarea).attr('title', errorMsg);
+                } else {
+                    $(textarea).removeAttr('title');
+                }
+            }
+            return !hasError;
+        });
+    }
