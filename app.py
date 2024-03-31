288146f2630f8aacc9fa5169f0320182ac6c1ec7
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-03-31 12:49:38 +0000

    debug off

--- app.py
+++ app.py
@@ -302,5 +302,5 @@ def not_found(e):
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")), debug=True)
+            port=int(os.environ.get("PORT")), debug=False)
 
